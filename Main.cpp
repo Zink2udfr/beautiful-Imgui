86a2a8c9406f0756b3080a4a465c17d69fb87e23
Zink <145686114+Zink2udfr@users.noreply.github.com>
2024-12-03 14:00:10 -0600

    Update

--- Main.cpp
+++ Main.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
 
+int main() { 
+  std::cout << "Tab = Aim ";
+  return 0;
+}
+
+int main() { 
+  std::cout << "Tab = Visuals ";
+  return 0;
+}
+
+int main() { 
+  std::cout << "Tab = Misc ";
+  return 0;
+}
+
+int main() { 
+  std::cout << "Tab = Config ";
+  return 0;
+}
+
+int main() { 
+
+  if std::cout = << "Tab = Aim "; {
+#include <MainGui.cpp> 
+  }
+}
